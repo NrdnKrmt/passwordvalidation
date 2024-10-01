e4b6414e35e0924f800048adea9353f7d4792232
NrdnKrmt <nourdinkaroumat@gmail.com>
2024-10-01 14:47:54 +0200

    Add: Extra tests (more than 8 characters, less than 8 characters and 0 characters)

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -5,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MainTest {
 
     @Test
+    @DisplayName("Exactly 8 characters")
    public void hasEightCharactersTest_when_password_expect_True() {
         //GIVEN
         String password = "password";
@@ -14,5 +16,45 @@ public class MainTest {
 
         //THEN
         assertTrue(actual);
+    }
+
+    //
+    @Test
+    @DisplayName("Less than 8 characters")
+    public void hasEightCharactersTest_when_word_expect_False() {
+        //GIVEN
+        String password = "word";
+
+        //WHEN
+        boolean actual = Main.hasEightCharacters(password);
+
+        //THEN
+        assertFalse(actual);
+    }
+
+    @Test
+    @DisplayName("More than 8 characters")
+    public void hasEightCharactersTest_when_SuperLongWord_expect_True() {
+        //GIVEN
+        String password = "SuperLongWord";
+
+        //WHEN
+        boolean actual = Main.hasEightCharacters(password);
+
+        //THEN
+        assertTrue(actual);
+    }
+
+    @Test
+    @DisplayName("0 characters")
+    public void hasEightCharactersTest_when___expect_True() {
+        //GIVEN
+        String password = "";
+
+        //WHEN
+        boolean actual = Main.hasEightCharacters(password);
+
+        //THEN
+        assertFalse(actual);
     }
 }
