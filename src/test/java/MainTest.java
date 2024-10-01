3af81aae732843f909054960bb24d9094db655c5
NrdnKrmt <nourdinkaroumat@gmail.com>
2024-10-01 14:34:05 +0200

    Add: Test for password length

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+    @Test
+   public void hasEightCharactersTest_when_password_expect_True() {
+        //GIVEN
+        String password = "password";
+
+        //WHEN
+        boolean actual = hasEightCharacters(password);
+
+        //THEN
+        assertTrue(actual);
+    }
+}
